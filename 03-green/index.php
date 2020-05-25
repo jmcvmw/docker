9cd42bf01abb78a19d7222b9382f99a1e02cf0e0
Jason Meers <git@jasonmeers.com>
2020-05-25 23:24:58 +0100

    fixed deletions

---
+++ 03-green/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML >
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>External style rules</title>
+<link rel="stylesheet" href="external.css" type="text/css" > 
+
+</head>
+<body>
+
+<h1>
+<?php 
+$ip_server = $_SERVER['SERVER_ADDR']; 
+$ip_port = $_SERVER['SERVER_PORT'];
+echo "Server: $ip_server : $ip_port"; 
+?></h1>
+
+<p style="background-color:green">03-GREEN</p>
+
+<h2>
+<?php 
+echo "Client: ";
+echo  $_SERVER['REMOTE_ADDR'];
+?></h2>
+
+</body>
+</html> 
